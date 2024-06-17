28cba48a2e85935bf60820f4b07d264ff4bd119c
vinca0224 <vinca0224@naver.com>
2024-06-17 12:36:02 +0900

    h2 설정까지

--- spring03/backboard/src/main/java/com/vinca/backboard/controller/MainController.java
+++ spring03/backboard/src/main/java/com/vinca/backboard/controller/MainController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -13,10 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
     
     @GetMapping("/hello")
-    @ResponseBody
     public String getHello() {
         log.info("getHello(); 실행");
-        return "Hello, Spring Boot3";
+        return "hello";
     }
     
 }
